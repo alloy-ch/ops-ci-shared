e9b6d00a3367d586e5477dca9d2d3274992746a1
Erik Jonsson Thorén <erik.jonsson@ringier.ch>
2025-02-19 18:44:55 +0100

    feat: change PR Link comment (#38)

--- scripts/ci-notify-github-pr-env-link.sh
+++ scripts/ci-notify-github-pr-env-link.sh
@@ -7,7 +7,7 @@ if [[ ${CODEBUILD_BUILD_SUCCEEDING} == "1" ]] && [[ -f /tmp/pr_env_url ]] && [[ 
   URL=$(cat /tmp/pr_env_url 2>/dev/null)
 
   export GH_TOKEN=$(aws --region "${AWS_REGION}" ssm get-parameter --output json --name /ops-ci/github-access-token --with-decryption | jq -crM '.Parameter.Value')
-  gh pr comment "$PR_NUMBER" -b "The PR environment is ready at ${URL} (commit \`${CODEBUILD_RESOLVED_SOURCE_VERSION}\`)" > /dev/null
+  gh pr comment "$PR_NUMBER" -b "The PR environment is ready at ${URL} (${ENV} @ \`${CODEBUILD_RESOLVED_SOURCE_VERSION}\`)" > /dev/null
 
 fi
 
